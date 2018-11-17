f0781df14cd9a2f2cf142558f2d18eb9eb496fe0
Jason Chang <jasonchang0@berkeley.edu>
2018-11-17 10:18:54 -0800

    completed homepage

--- __init__.py
+++ __init__.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+    return render_template('main.html')
+
+
+@app.route('/dashboard/')
+def dashboard():
+    # return 'This is dashboard.'
+    return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
